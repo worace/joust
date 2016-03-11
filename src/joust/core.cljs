bb6aac400865082b09168b48b4e02ef13f9f21d8
Horace Williams <horace.d.williams@gmail.com>
2016-03-11 09:43:55 -0700

    working out some movement/drawing fundamentals

--- src/joust/core.cljs
+++ src/joust/core.cljs
@@ -7,7 +7,10 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Joust"}))
+(def app-state (atom {:text "Joust"
+                      :character {:position [300 300]
+                                  :velocity [5 0]}
+                      :game-size [800 600]}))
 
 (defn by-id [id] (.getElementById js/document id))
 
@@ -17,10 +20,33 @@
 
 (defn draw-rect [ctx x y w h] (.fillRect ctx x y w h))
 
-(defn clear-canvas [ctx] (.clearRect ctx 0 0 400 400))
+(defn clear-canvas [ctx] (.clearRect ctx 0 0 800 600))
+
+(defn move-character [{:keys [velocity] :as character}]
+  (update-in character [:position] (partial map + velocity)))
+
+(defn draw-game [app-state canvas-ctx]
+  (let [[char-x char-y] (:position (:character app-state))]
+    (draw-rect canvas-ctx char-x char-y 50 50)))
+
+(defn wrap [bounds coordinates]
+  (map mod coordinates bounds))
+
+(defn game-tick [app-state]
+  ;; (println "game tick state: " app-state)
+  (-> app-state
+      (update-in [:character] move-character)
+      (update-in [:character :position] (partial wrap (:game-size app-state)))))
+
+;; drawing platforms
+;; gravitation
+;; detect platform collision (gravitation end)
 
 (defn draw-fn []
-  (draw-rect canvas-ctx 50 50 50 50)
+  ;; update state
+  (clear-canvas canvas-ctx)
+  (swap! app-state game-tick)
+  (draw-game @app-state canvas-ctx)
   (js/setTimeout (fn [] (.requestAnimationFrame js/window draw-fn))
                  70))
 
